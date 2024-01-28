60aa8f9f9e245c668feb28e5db4fde54eda22696
ytono <yasumoto-tonoshiro@vorn.co.jp>
2024-01-28 10:03:08 +0900

    added sleep 10.

--- 202401/Trivia/Week02/03/a.sh
+++ 202401/Trivia/Week02/03/a.sh
@@ -3,6 +3,8 @@ read -p "Region : " REGION
 gcloud services enable run.googleapis.com
 gcloud services enable cloudfunctions.googleapis.com
 
+sleep 10
+
 mkdir gc-function
 mv index.js package.json gc-function
 
@@ -14,6 +16,3 @@ curl -m 70 -X POST https://$REGION-$DEVSHELL_PROJECT_ID.cloudfunctions.net/GCFun
 -H "Authorization: bearer $(gcloud auth print-identity-token)" \
 -H "Content-Type: application/json" \
 -d '{"message":"Hello World!"}'
-
-##
-gcloud logging read --project $DEVSHELL_PROJECT_ID --limit 10 --filter "resource.type=cloud_function AND resource.labels.function_name=GCFunction"
